26f96e69f6bbd8c57049d76e81aeb112744db153
Local User <mariano-jose1926@hotmail.com>
2024-05-22 02:04:52 -0500

    datosPrimitivos clase#3

---
+++ Tipos_De_Datos/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args){
+        int dato1 = 1;
+        byte dato2 = 2;
+        short dato3 = 3;
+        long dato4 = 4;
+        float dato5 = 5f;
+        double dato6 = 6;
+        char dato7 = '7';
+
+        System.out.println("Tipo de dato INT Tiene el valor de: "+dato1);
+        System.out.println("Valor maximo = "+ Integer.MAX_VALUE);
+        System.out.println("Valor minimo = "+ Integer.MIN_VALUE+"\n");
+
+        System.out.println("Tipo de dato byte Tiene el valor de: "+dato2);
+        System.out.println("Valor maximo = "+ Byte.MAX_VALUE);
+        System.out.println("Valor minimo = "+ Byte.MIN_VALUE+"\n");
+
+        System.out.println("Tipo de dato shot Tiene el valor de: "+dato3);
+        System.out.println("Valor maximo = "+ Short.MAX_VALUE);
+        System.out.println("Valor minimo = "+ Short.MIN_VALUE+"\n");
+
+        System.out.println("Tipo de dato long Tiene el valor de: "+dato4);
+        System.out.println("Valor maximo = "+ Long.MAX_VALUE);
+        System.out.println("Valor minimo = "+ Long.MIN_VALUE+"\n");
+
+        System.out.println("Tipo de dato Float Tiene el valor de: "+dato5);
+        System.out.println("Valor maximo = "+ Float.MAX_VALUE);
+        System.out.println("Valor minimo = "+ Float.MIN_VALUE+"\n");
+
+        System.out.println("Tipo de dato double Tiene el valor de: "+dato6);
+        System.out.println("Valor maximo = "+ Double.MAX_VALUE);
+        System.out.println("Valor minimo = "+ Double.MIN_VALUE+"\n");
+
+        System.out.println("Tipo de dato Chard Tiene el valor de: "+dato7);
+        System.out.println("Valor maximo = "+ Character.MAX_VALUE);
+        System.out.println("Valor minimo = "+ Character.MIN_VALUE+"\n");
+
+    }
+}
